9897392e94a537aef1d70760fd31ce39838ddb41
dapaalpinnn <dafaalvin10@gmail.com>
2025-05-24 19:53:51 +0700

    chore(home): increase article fetch count from 5 to 6 for pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $articles = Article::latest()->take(5)->get();
+        $articles = Article::latest()->take(6)->get();
         return view('home', compact('articles'));
     }
 }
